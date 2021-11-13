d5b15527e2beaaaeecfd5172c0642f3a110d6e20
Jesus Arath <jesus.flores.p19@gmail.com>
2021-11-12 21:09:09 -0600

    Agregamos moduio de firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,27 @@ import { SwiperModule } from 'swiper/angular';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+//Angular Fire
+import { environment } from "../environments/environment"
+import { AngularFireModule } from '@angular/fire/compat';
+
+//Angular Material
+import { MatSelectModule } from "@angular/material/select";
+import { MatInputModule } from "@angular/material/input"
+import { NgxMatSelectSearchModule } from 'ngx-mat-select-search';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatAutocompleteModule} from '@angular/material/autocomplete';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { provideFirestore,getFirestore } from '@angular/fire/firestore';
+
+const AngularMaterialModule: any = [
+  MatSelectModule,
+  MatInputModule,
+  MatFormFieldModule,
+  MatAutocompleteModule
+]
 
 @NgModule({
   declarations: [
@@ -13,8 +34,18 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    
+    FormsModule,
+    ReactiveFormsModule,
+    
+    AngularFireModule.initializeApp(environment.firebase),
+    
     SwiperModule,
-    BrowserAnimationsModule
+    //Angular Material
+    BrowserAnimationsModule,
+    AngularMaterialModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideFirestore(() => getFirestore())
   ],
   providers: [],
   bootstrap: [AppComponent]
