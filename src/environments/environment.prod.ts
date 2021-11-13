cb9424505271d34ee502956916bbf0b1536fc08c
Jesus Arath <jesus.flores.p19@gmail.com>
2021-11-12 20:08:50 -0600

    configuracion en produccion

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
+  firebase: {
+    projectId: 'terminales-cc1c5',
+    appId: '1:205855274483:web:b4d8d73992a167dec88e86',
+    storageBucket: 'terminales-cc1c5.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyBx3skrryKQrR722kr3hEGir2tsA4h_u1M',
+    authDomain: 'terminales-cc1c5.firebaseapp.com',
+    messagingSenderId: '205855274483',
+  },
   production: true
 };
