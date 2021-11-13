ca36805cf0f98cbfab00b656f291c9b9e95cc48c
Jesus Arath <jesus.flores.p19@gmail.com>
2021-11-12 20:10:38 -0600

    Creamos interfaz de tiendas

---
+++ src/app/interface/tiendas.interface.ts
@@ -0,0 +1,11 @@
+export interface Tienda {
+    SAP:         string;
+    ACTIVO:      number;
+    TIENDA:      string;
+    REGION:      string;
+    TERRITORIO:  string;
+    SUBDIRECTOR: string;
+    COORDINADOR: string;
+    STATUS:      string;
+    IDPDV:       number;
+}
