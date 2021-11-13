53f3a117a60312f0d7a42515114587eaf01a07b5
Jesus Arath <jesus.flores.p19@gmail.com>
2021-11-12 20:07:59 -0600

    configuracion en desarollo

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    projectId: 'terminales-cc1c5',
+    appId: '1:205855274483:web:b4d8d73992a167dec88e86',
+    storageBucket: 'terminales-cc1c5.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyBx3skrryKQrR722kr3hEGir2tsA4h_u1M',
+    authDomain: 'terminales-cc1c5.firebaseapp.com',
+    messagingSenderId: '205855274483',
+  },
 };
 
 /*
