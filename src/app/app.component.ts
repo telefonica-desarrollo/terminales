5a731121e4b01b770b5c5b2e0388407f64fad2d3
Jesus Arath <jesus.flores.p19@gmail.com>
2021-11-12 22:23:24 -0600

    Guardar en el localstorage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import {FormControl} from '@angular/forms';
 import {Observable} from 'rxjs';
-import {map, startWith} from 'rxjs/operators';
+import {map, retry, startWith} from 'rxjs/operators';
 import { Tienda } from './interface/tiendas.interface';
 import { TiendasService } from './services/tiendas.service';
 
@@ -20,36 +20,10 @@ export interface State {
 export class AppComponent {
   title = 'proyecto';
   stateCtrl = new FormControl();
-  filteredStates: Observable<State[]>;
-  filtroTiendas: Observable<Tienda[]>;
 
-  states: State[] = [
-    {
-      name: 'Arkansas',
-      population: '2.978M',
-      // https://commons.wikimedia.org/wiki/File:Flag_of_Arkansas.svg
-      flag: 'https://upload.wikimedia.org/wikipedia/commons/9/9d/Flag_of_Arkansas.svg',
-    },
-    {
-      name: 'California',
-      population: '39.14M',
-      // https://commons.wikimedia.org/wiki/File:Flag_of_California.svg
-      flag: 'https://upload.wikimedia.org/wikipedia/commons/0/01/Flag_of_California.svg',
-    },
-    {
-      name: 'Florida',
-      population: '20.27M',
-      // https://commons.wikimedia.org/wiki/File:Flag_of_Florida.svg
-      flag: 'https://upload.wikimedia.org/wikipedia/commons/f/f7/Flag_of_Florida.svg',
-    },
-    {
-      name: 'Texas',
-      population: '27.47M',
-      // https://commons.wikimedia.org/wiki/File:Flag_of_Texas.svg
-      flag: 'https://upload.wikimedia.org/wikipedia/commons/f/f7/Flag_of_Texas.svg',
-    },
-  ];
+  filtroTiendas: Observable<Tienda[]>;
   tiendas: Tienda[] = []
+  tiendaSeleccionada: any;
 
   valor: any;
 
@@ -58,24 +32,32 @@ export class AppComponent {
     //   this.tiendas = data as Tienda[]
     //   console.log(this.tiendas);
     // })
+
     this.tiendas = this._tiendas.tiendas;
+    this.stateCtrl.valueChanges.subscribe((tienda)=> {
+      this.tiendaSeleccionada = null;
+      this.tiendas.forEach((btienda)=> {
+        if(btienda.IDPDV === tienda){
+          this.tiendaSeleccionada = btienda
+          localStorage.setItem("idpdv", this.tiendaSeleccionada.IDPDV)
+        }
+      })
+    })
+
     this.filtroTiendas = this.stateCtrl.valueChanges.pipe(
       startWith(""),
-      map(tienda => (tienda? this._filtroTiendas(tienda) : this.tiendas.slice()))
+      map(tienda => this._filtroTiendas(tienda)),
     )
+
+    if(localStorage.getItem("idpdv")){
+      this.stateCtrl.setValue(Number(localStorage.getItem("idpdv")))
+    }
+
   }
 
   private _filtroTiendas(value: string): Tienda[] {
     const filtroValor = value.toString().toLowerCase();
-    console.log(this.tiendas.filter(tienda => tienda.IDPDV.toString().includes(filtroValor)
-                                            || tienda.TIENDA.includes(filtroValor)
-    ));
-    
     return this.tiendas.filter(tienda => tienda.IDPDV.toString().includes(filtroValor) || tienda.TIENDA.toLowerCase().includes(filtroValor))
   }
 
-  private _filterStates(value: string): State[] {
-    const filterValue = value.toLowerCase();
-    return this.states.filter(state => state.name.toLowerCase().includes(filterValue));
-  }
 }
