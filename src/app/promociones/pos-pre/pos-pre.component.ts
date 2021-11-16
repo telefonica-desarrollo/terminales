7f589f6a34d18ad4d85e83c70d1807950ac465d7
Jesus Arath <jesus.flores.p19@gmail.com>
2021-11-16 11:00:42 -0600

    Agregamos componente pos-pre

---
+++ src/app/promociones/pos-pre/pos-pre.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pos-pre',
+  templateUrl: './pos-pre.component.html',
+  styleUrls: ['./pos-pre.component.css']
+})
+export class PosPreComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
