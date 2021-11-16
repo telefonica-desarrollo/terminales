04a13d17e8dcf512710f633b26676d3fcf89f4e9
Jesus Arath <jesus.flores.p19@gmail.com>
2021-11-16 10:55:10 -0600

    Se agrego componente prepago

---
+++ src/app/promociones/prepago/prepago.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-prepago',
+  templateUrl: './prepago.component.html',
+  styleUrls: ['./prepago.component.css']
+})
+export class PrepagoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
