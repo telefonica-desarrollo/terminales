26b878021d361fa02735786158f8867d8b177938
Jesus Arath <jesus.flores.p19@gmail.com>
2021-11-16 11:00:24 -0600

    Agregamos componente pospago

---
+++ src/app/promociones/pospago/pospago.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pospago',
+  templateUrl: './pospago.component.html',
+  styleUrls: ['./pospago.component.css']
+})
+export class PospagoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
