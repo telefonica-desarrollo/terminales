10f1d1e4fb0c7ae1131602b516c00680ad7f712e
Jesus Arath <jesus.flores.p19@gmail.com>
2021-11-16 11:01:33 -0600

    Creamos rutas de navegacion

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {PrepagoComponent} from "./promociones/prepago/prepago.component"
+import {PospagoComponent} from "./promociones/pospago/pospago.component"
+import {PosPreComponent} from "./promociones/pos-pre/pos-pre.component"
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "prepago", component: PrepagoComponent},
+  {path: "pospago", component: PospagoComponent},
+  {path: "prepago", component: PosPreComponent},
+  {path: "**", redirectTo: "prepago"}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
