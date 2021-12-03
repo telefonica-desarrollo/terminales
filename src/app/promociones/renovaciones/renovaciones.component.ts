aab10f008488e06d506ae866b547802b13b2a3ff
Jesus Arath <jesus.flores.p19@gmail.com>
2021-12-03 11:31:49 -0600

    Nuevo diseño de renovaciones

---
+++ src/app/promociones/renovaciones/renovaciones.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-renovaciones',
+  templateUrl: './renovaciones.component.html',
+  styleUrls: ['./renovaciones.component.css']
+})
+export class RenovacionesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
