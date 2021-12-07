42d0f71ccc23df1d6b83b32130c81e4442e32a27
Jesus Arath <jesus.flores.p19@gmail.com>
2021-12-06 21:09:51 -0600

    Pipe de comisiones

--- src/app/pipes/comision.pipe.ts
+++ src/app/pipes/comision.pipe.ts
@@ -5,8 +5,9 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class ComisionPipe implements PipeTransform {
 
-  transform(value: number): number {
-    const pvp = value
+  transform(value: number, descuento: number): number {
+    const pvp_inicial = value
+    const pvp = pvp_inicial + (pvp_inicial * descuento)
     const pvp_sin_iva = pvp - (pvp * 0.16)
     const comision = pvp_sin_iva * 0.02
 
