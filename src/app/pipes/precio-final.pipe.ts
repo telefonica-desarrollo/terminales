05bbb0e23235f91815b97b2b37e88987bb626706
Jesus Arath <jesus.flores.p19@gmail.com>
2021-12-06 21:10:04 -0600

    Pipe de precio final

---
+++ src/app/pipes/precio-final.pipe.ts
@@ -0,0 +1,14 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'precioFinal'
+})
+export class PrecioFinalPipe implements PipeTransform {
+
+  transform(value: number, descuento: number): number | null {
+    const precio_inicial = value;
+    const precio_final = precio_inicial + (descuento * precio_inicial)
+    return precio_final;
+  }
+
+}
