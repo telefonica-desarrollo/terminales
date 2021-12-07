25f162a0bf998fa7de3c140366da15995fa6231a
Jesus Arath <jesus.flores.p19@gmail.com>
2021-12-06 21:09:15 -0600

    Nuevas Rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 import {PrepagoComponent} from "./promociones/prepago/prepago.component"
 import {PospagoComponent} from "./promociones/pospago/pospago.component"
 import {PosPreComponent} from "./promociones/pos-pre/pos-pre.component"
+import { RenovacionesComponent } from './promociones/renovaciones/renovaciones.component';
 
 const routes: Routes = [
   {path: "prepago", component: PrepagoComponent},
   {path: "pospago", component: PospagoComponent},
-  {path: "prepos", component: PosPreComponent},
+  {path: "renovaciones", component: RenovacionesComponent},
   {path: "**", redirectTo: "prepago"}
 ];
 
