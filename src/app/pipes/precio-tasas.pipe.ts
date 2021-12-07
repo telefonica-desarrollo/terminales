24cd61ad71df45b9d524271202e2a467c2b88491
Jesus Arath <jesus.flores.p19@gmail.com>
2021-12-06 23:21:27 -0600

    Modificamos pipe de tasas

--- src/app/pipes/precio-tasas.pipe.ts
+++ src/app/pipes/precio-tasas.pipe.ts
@@ -5,9 +5,10 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class PrecioTasasPipe implements PipeTransform {
 
-  transform(value: number, tasa: number): number{
-    const precio_con_tasa = value + (value*tasa)
-    return precio_con_tasa;
+  transform(value: number, tasa: number, descuento = 0): number{
+      const precio_con_tasa = value + (value*tasa)
+      const precio_tasa_des = precio_con_tasa + (precio_con_tasa * descuento)
+      return precio_tasa_des;
   }
 
 }
