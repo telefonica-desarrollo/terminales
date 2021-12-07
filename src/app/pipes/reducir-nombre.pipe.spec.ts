2a4abb11826879e9f7ec070751d3902de9081797
Jesus Arath <jesus.flores.p19@gmail.com>
2021-12-06 23:20:49 -0600

    Eliminamos documento de pruebas

--- src/app/pipes/reducir-nombre.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { ReducirNombrePipe } from './reducir-nombre.pipe';
-
-describe('ReducirNombrePipe', () => {
-  it('create an instance', () => {
-    const pipe = new ReducirNombrePipe();
-    expect(pipe).toBeTruthy();
-  });
-});
