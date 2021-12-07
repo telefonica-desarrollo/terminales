ed1eddcac173d7fed4468dd68e515b7bed845489
Jesus Arath <jesus.flores.p19@gmail.com>
2021-12-06 21:09:27 -0600

    Algoritmo de ordenamiento

--- src/app/promociones/renovaciones/renovaciones.component.ts
+++ src/app/promociones/renovaciones/renovaciones.component.ts
@@ -1,15 +1,64 @@
 import { Component, OnInit } from '@angular/core';
+import {animate, state, style, transition, trigger} from '@angular/animations';
+import { Promociones }  from "../../interface/tiendas.interface"
+import { TerminalesService } from 'src/app/services/terminales.service';
 
 @Component({
   selector: 'app-renovaciones',
   templateUrl: './renovaciones.component.html',
-  styleUrls: ['./renovaciones.component.css']
+  styleUrls: ['./renovaciones.component.css'],
+  animations: [
+    trigger('detailExpand', [
+      state('collapsed', style({height: '0px', minHeight: '0'})),
+      state('expanded', style({height: '*'})),
+      transition('expanded <=> collapsed', animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')),
+    ]),
+  ],
 })
+
 export class RenovacionesComponent implements OnInit {
 
-  constructor() { }
+  constructor(private terminales: TerminalesService) {
+    this.data = this.terminales.obtenerPromocionesRenovaciones()
+    
+    //Ordenar data
+    this.data.sort((a: any , b: any )=> {
+      if(a.PORCENTAJE == b.PORCENTAJE) return 0
+      if(a.PORCENTAJE > b.PORCENTAJE ) return 1
+      return -1
+    })
+    //Obtener Catalogo de marcas
+    for(let promo of this.data){
+      let validar = true
+      for(let marca of this.marcas){
+        if(marca == promo.MARCA) validar = false
+      }
+      if(validar) this.marcas.push(promo.MARCA)
+    }
+    this.dataSave = this.data
+    
+  }
+  data: any = [];
+  dataSave: any = []
+  marcas: any = [];
+
+  
+  columnsToDisplay2 = ['MODELO', 'PRECIO_INICIAL', 'PRECIO_FINAL', 'COMISION'];
+  expandedElement: Promociones[] | null;
 
   ngOnInit(): void {
   }
+
+  filtroMarca(marca: any){
+    this.data = []
+    if(marca == "Todas") this.data  = this.dataSave
+    else{
+      this.dataSave.forEach((promocion: any) => {
+        if(promocion.MARCA == marca) this.data.push(promocion)
+      });
+    }
+    console.table(this.data)
+  }
+
 
 }
