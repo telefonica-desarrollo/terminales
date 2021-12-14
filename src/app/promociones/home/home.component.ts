d74c82ef496ff7bb8f96152d3b2737f02344bd12
Jesus Arath <jesus.flores.p19@gmail.com>
2021-12-14 16:24:04 -0600

    Estrucutra de app sera la del componente home

--- src/app/promociones/home/home.component.ts
+++ src/app/promociones/home/home.component.ts
@@ -1,4 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl} from '@angular/forms';
+import {Observable} from 'rxjs';
+import {map, retry, startWith} from 'rxjs/operators';
+
+
+import { Tienda } from './../../interface/tiendas.interface';
+import { TiendasService } from './../../services/tiendas.service';
+
+export interface State {
+  flag: string;
+  name: string;
+  population: string;
+}
+
 
 @Component({
   selector: 'app-home',
@@ -7,7 +21,51 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  title = 'proyecto';
+  stateCtrl = new FormControl();
+
+  filtroTiendas: Observable<Tienda[]>;
+  tiendas: Tienda[] = []
+  tiendaSeleccionada: any;
+
+  valor: any;
+
+  constructor(public _tiendas: TiendasService) {
+    // this._tiendas.obtenerTiendas().subscribe((data)=>{
+    //   this.tiendas = data as Tienda[]
+    //   console.log(this.tiendas);
+    // })
+
+    this.tiendas = this._tiendas.tiendas;
+    console.log(this.tiendas);
+    
+    
+    this.stateCtrl.valueChanges.subscribe((tienda)=> {
+      this.tiendaSeleccionada = null;
+      this.tiendas.forEach((btienda)=> {
+        if(btienda.IDPDV === tienda){
+          this.tiendaSeleccionada = btienda
+          localStorage.setItem("idpdv", this.tiendaSeleccionada.IDPDV)
+        }
+      })
+    })
+
+    this.filtroTiendas = this.stateCtrl.valueChanges.pipe(
+      startWith(""),
+      map(tienda => this._filtroTiendas(tienda)),
+    )
+
+    if(localStorage.getItem("idpdv")){
+      this.stateCtrl.setValue(Number(localStorage.getItem("idpdv")))
+    }
+
+  }
+
+  private _filtroTiendas(value: string): Tienda[] {
+    const filtroValor = value.toString().toLowerCase();
+    return this.tiendas.filter(tienda => tienda.IDPDV.toString().includes(filtroValor) || tienda.TIENDA?.toLowerCase().includes(filtroValor))
+  }
+
 
   ngOnInit(): void {
   }
