353164ddbb355835861227038da9603f87f01187
Jesus Arath <jesus.flores.p19@gmail.com>
2021-12-13 18:41:26 -0600

    Estructura logica de nuevos filtros y data guardada

--- src/app/promociones/renovaciones/renovaciones.component.ts
+++ src/app/promociones/renovaciones/renovaciones.component.ts
@@ -27,16 +27,21 @@ export class RenovacionesComponent implements OnInit {
       if(a.PORCENTAJE > b.PORCENTAJE ) return 1
       return -1
     })
-    //Obtener Catalogo de marcas
-    for(let promo of this.data){
-      let validar = true
-      for(let marca of this.marcas){
-        if(marca == promo.MARCA) validar = false
-      }
-      if(validar) this.marcas.push(promo.MARCA)
-    }
     this.dataSave = this.data
-    
+    this.obtenerCatalogo()
+
+    //Obtener Data guardada
+    if(localStorage.getItem("tasa_renovacion")){
+      this.valorTasas = localStorage.getItem("tasa_renovacion")
+      this.filtroTasa(this.valorTasas)
+    }
+    if(localStorage.getItem("marca_renovacion")){
+      this.valorMarcas = localStorage.getItem("marca_renovacion")
+      this.filtroMarca(this.valorMarcas)
+    }
+    if(localStorage.getItem("payjoy_renovacion")){
+      this.valorPayjoy = localStorage.getItem("payjoy_renovacion")
+    }
   }
 
   data: any = [];
@@ -45,24 +50,52 @@ export class RenovacionesComponent implements OnInit {
   mensajeTasa: string = "De contado";
   valueTasa: number = 0;
 
-  
+  valorPayjoy: any= false
+  valorMarcas: string | null = ""
+  valorTasas: string | null = "0"
+  valorModelo= ""
+
   columnsToDisplay2 = ['MODELO', 'PRECIO_INICIAL', 'PRECIO_FINAL', 'COMISION'];
   expandedElement: Promociones[] | null;
 
   ngOnInit(): void {
   }
 
-  filtroMarca(marca: any){
-    this.data = []
-    if(marca == "Todas") this.data  = this.dataSave
-    else{
-      this.dataSave.forEach((promocion: any) => {
-        if(promocion.MARCA == marca) this.data.push(promocion)
-      });
+  obtenerCatalogo(){
+    //Obtener Catalogo de marcas
+    this.marcas = []
+    for(let promo of this.data){
+      let validar = true
+      for(let marca of this.marcas){
+        if(marca == promo.MARCA) validar = false
+      }
+      if(validar) this.marcas.push(promo.MARCA)
     }
-    console.table(this.data)
+    //Ordenar Catalogo de marcas
+    this.marcas.sort()
   }
 
+  filtroModelo(){
+    const modelo = this.valorModelo.toLowerCase();
+    this.data = this.dataSave.filter((data: any) => data.MODELO.toLowerCase().includes(modelo))
+    this.obtenerCatalogo();
+  }
+  filtroMarca(marca: any){
+    this.data = []
+    const modelo = this.valorModelo.toLowerCase();
+
+    if(marca == "Todas"){
+      this.data  = this.dataSave
+      this.filtroModelo()
+    }
+    else{
+      this.dataSave.forEach((promocion: any) => {
+        if(promocion.MARCA == marca && 
+          promocion.MODELO.toLowerCase().includes(modelo)) this.data.push(promocion)
+      });
+    }
+    localStorage.setItem("marca_renovacion", marca);
+  }
   filtroTasa(tasa: any){
     if(tasa == 0){
       this.mensajeTasa = "de contado"
@@ -70,17 +103,22 @@ export class RenovacionesComponent implements OnInit {
     }
     if(tasa == 12){
       this.mensajeTasa = "a 12 meses"
-      this.valueTasa = 0.06
+      this.valueTasa = 0.09
     }
     if(tasa == 18){
       this.mensajeTasa = "a 18 meses"
-      this.valueTasa = 0.08
+      this.valueTasa = 0.14
     }
     if(tasa == 24){
       this.mensajeTasa = "a 24 meses"
-      this.valueTasa = 0.10
+      this.valueTasa = 0.20
     }
+    localStorage.setItem("tasa_renovacion", tasa);
   }
-
-
+  filtroPayjoy(){
+    localStorage.setItem("payjoy_renovacion", this.valorPayjoy);
+  }
+  
+  
+  
 }
