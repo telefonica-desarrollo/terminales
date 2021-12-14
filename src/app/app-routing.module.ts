f61f8db1d983fb6eda7b65c6ee3386519fe65dd6
Jesus Arath <jesus.flores.p19@gmail.com>
2021-12-14 16:04:37 -0600

    Agregamos rutas de nuevos componentes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,14 +2,18 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {PrepagoComponent} from "./promociones/prepago/prepago.component"
 import {PospagoComponent} from "./promociones/pospago/pospago.component"
-import {PosPreComponent} from "./promociones/pos-pre/pos-pre.component"
 import { RenovacionesComponent } from './promociones/renovaciones/renovaciones.component';
+import { HomeComponent } from './promociones/home/home.component';
+import { LoginComponent } from './login/login.component';
+
 
 const routes: Routes = [
-  {path: "prepago", component: PrepagoComponent},
-  {path: "pospago", component: PospagoComponent},
-  {path: "renovaciones", component: RenovacionesComponent},
-  {path: "**", redirectTo: "prepago"}
+  {path: "login", component: LoginComponent},
+  {path: "home", component: HomeComponent, children: [
+    {path: "prepago", component: PrepagoComponent},
+    {path: "pospago", component: PospagoComponent},
+    {path: "renovaciones", component: RenovacionesComponent},
+  ]}
 ];
 
 @NgModule({
