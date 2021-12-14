d7f00b8fea53849a202273eeba2d9f7231c590c5
Jesus Arath <jesus.flores.p19@gmail.com>
2021-12-13 22:28:22 -0600

    Precio mensual de terminal

--- src/app/pipes/precio-mensual.pipe.ts
+++ src/app/pipes/precio-mensual.pipe.ts
@@ -5,8 +5,11 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class PrecioMensualPipe implements PipeTransform {
 
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+  transform(value: number, tasa: number, descuento: number, meses: number): number{
+    const precio_con_tasa = value + (value*tasa)
+    const precio_tasa_des = precio_con_tasa + (precio_con_tasa * descuento)
+    const precio_mensual = precio_tasa_des / meses
+    return precio_mensual;
   }
 
 }
