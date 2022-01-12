d7d6fdeaa6e437a372749804ea84dce6d2fcfd3c
Jesus Arath <jesus.flores.p19@gmail.com>
2022-01-11 20:45:44 -0600

    Diseño del card renovaciones y demas. This resolves #10

--- src/app/cargar-prom/promociones-renovacion/promociones-renovacion.component.ts
+++ src/app/cargar-prom/promociones-renovacion/promociones-renovacion.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as Exceljs from "exceljs"
 
 @Component({
   selector: 'app-promociones-renovacion',
@@ -6,10 +7,52 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./promociones-renovacion.component.css']
 })
 export class PromocionesRenovacionComponent implements OnInit {
+  
+  nombreDocumento: string = ""
+  error: boolean = false;
+  statusTexto: string = "Documento Leído"
+  status: number = 0;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+  obtenerArchivo(event: any){
+    const file = event.target.files[0];
+    const reader = new FileReader();
+
+    reader.readAsArrayBuffer(file)
+    reader.onload = () => {
+      const buffer: any = reader.result;
+      let woorkbook = new Exceljs.Workbook();
+      
+      woorkbook.xlsx.load(buffer).then((err)=>{
+        var woorksheet = woorkbook.getWorksheet("Hoja1");
+        woorksheet.eachRow((row, rowNumber) => {
+          if(rowNumber>1){
+            this.nombreDocumento = file.name
+            const nombre = row.getCell(1).value;
+            const apellido = row.getCell(2).value;
+            console.log({nombre, apellido});
+          }
+        })
+      }).catch(()=> {
+        this.error= true 
+        this.nombreDocumento=""
+      })
+
+    }
+    
+    
+  }
+  badgeClass(){
+    if(this.status == 0) return "bg-secondary"
+    if(this.status == 1) return "bg-success"
+    if(this.status == 2) return "bg-danger"
+    return ""
+  }
+  cargarData(){
+    this.status=1 
   }
 
 }
